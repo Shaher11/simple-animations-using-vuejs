05beddaf7d715a78a444a10e46a2dc85c4210035
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-03-19 23:45:25 +0200

    Creating animation using JS

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,41 @@
             <button class="btn btn-primary" @click="show = !show">show</button>
             <br><br>
             <transition name="fade">
+                <div class="alert alert-info"  v-show="show">
+                    This is some info
+                </div>   
+            </transition>
+
+            <transition name="slide" type="animation" appear>
                 <div class="alert alert-info"  v-if="show">
                     This is some info
+                </div>   
+            </transition>
+             <transition 
+                enter-active-class="animate__animated animate__wobble"
+                leave-active-class="animate__animated animate__swing"
+             >
+                <div class="alert alert-info"  v-show="show">
+                    This is some info
+                </div>   
+            </transition>
+            <hr>
+            <button class="btn btn-primary" @click="load= !load"> Load / Remove Element</button>
+             <br><br>
+            <transition 
+                @before-enter="beforeEnter"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @enter-cancelled="enterCancelled"
+
+                @before-leave="beforeLeave"
+                @leave="leave"
+                @after-leave="afterLeave"
+                @leave-cancelled="leaveCancelled"
+                :css="false"
+            >
+                <div style="width:300px; height:100px; background-color:lightgreen"  v-if="load">
+                    
                 </div>   
             </transition>
 
@@ -25,9 +58,63 @@ export default {
     data() {
         return {
             show: false,
+            load: true,
+            alertAnimation: 'fade',
+            elementWidth: 100
            
         };
     },
+    methods:{
+        beforeEnter(el){
+            console.log('beforeEnter');
+            this.elementWidth = 100;
+            el.style.width = this.elementWidth + 'px';
+            
+        },
+        enter(el,done){
+            console.log('enter');
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = (this.elementWidth + round * 10) + 'px';
+                round++;
+                if(round > 20){
+                    clearInterval(interval);
+                    done();
+                }
+            }, 20);
+        },
+        afterEnter(el){
+            console.log('afterEnter');
+        },
+        enterCancelled(el){
+            console.log('enterCancelled');
+        },
+
+        beforeLeave(el){
+            console.log('beforeLeave');
+            this.elementWidth = 300;
+            el.style.width = this.elementWidth + 'px';
+        },
+        leave(el,done){
+            console.log('leave');
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = (this.elementWidth - round * 10) + 'px';
+                round++;
+                if(round > 20){
+                    clearInterval(interval);
+                    done();
+                }
+            }, 20);
+        },
+        afterLeave(el){
+            console.log('afterLeave');
+        },
+        leaveCancelled(el){
+            console.log('leaveCancelled');
+        },
+
+    }
     
 
 };
@@ -47,5 +134,40 @@ export default {
         transition: opacity 1s;
         opacity: 0;
     }
+
+    .slide-enter{
+        opacity: 0;
+        /* transform: translateY(20px); */
+    }
+    .slide-enter-active{
+        transition: opacity 1s;
+        animation: slide-in 1s ease-out forwards;
+    }
+    .slide-leave{
+    
+    }
+    .slide-leave-active{
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 3s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from{
+            transform: translateY(20px);
+        }
+        to{
+            transform: translateY(0);
+        }
+    }
+    
+    @keyframes slide-out {
+        from{
+            transform: translateY(0);
+        }
+        to{
+            transform: translateY(20px);
+        }
+    }   
 
 </style>
